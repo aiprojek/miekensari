876db151819bd9568b4a959b25d8318df8ddcb6d
aiprojek <71024676+aiprojek@users.noreply.github.com>
2023-03-16 19:07:53 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/*!
+* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+*/
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
+    scrollTriggerList.map(scrollTrigger => {
+        scrollTrigger.addEventListener('click', () => {
+            sidebarWrapper.classList.remove('active');
+            menuToggle.classList.remove('active');
+            _toggleMenuIcon();
+        })
+    });
